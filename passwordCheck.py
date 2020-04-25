3298023bc93a2a6414dec049b2e777d8072bd02d
SwarajEPICoder <64256098+SwarajEPICoder@users.noreply.github.com>
2020-04-25 11:34:15 +0200

    Add files via upload

---
+++ passwordCheck.py
@@ -0,0 +1,12 @@
+password_input = input('Enter your password: ')
+password = 'food'
+password2 = 'yummy'
+
+
+if password_input == password:
+    print('Yay! That was the right passord!')
+elif password_input == password2:
+    print('That was password2')
+else:
+    print('No! That wasn\'t the right password')
+
